29277f5d784d119698848c24f04f7e660fcb7c40
Jon Nardini <jwnardine@gmail.com>
2016-05-16 15:49:32 -0700

    add form.vtl page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,7 +13,7 @@ public class App{
 
     get("/", (request, response) ->{
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
+      model.put("template", "templates/form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
