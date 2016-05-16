d311d6b4569f009eea0ca9f84787a4c5e81cc810
rh <ryanjharvey88@gmail.com>
2016-05-16 09:06:51 -0700

    Initial commit

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,19 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/geneology_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteUsersQuery = "DELETE FROM users *;";
+      con.createQuery(deleteUsersQuery).executeUpdate();
+    }
+  }
+
+}
