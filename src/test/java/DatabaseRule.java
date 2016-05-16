862cf03e7c71a58839568cf4cb1c2e8b83d622a5
rh <ryanjharvey88@gmail.com>
2016-05-16 13:25:24 -0700

    Fixed small bugs

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,14 +5,16 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/geneology_test", "Ryan", "dirtyhands");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/geneology_test", null, null);
   }
 
   @Override
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
       String deleteUsersQuery = "DELETE FROM users *;";
+      String deleteRelativesQuery = "DELETE FROM relatives *;";
       con.createQuery(deleteUsersQuery).executeUpdate();
+      con.createQuery(deleteRelativesQuery).executeUpdate();
     }
   }
 
