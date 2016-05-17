78a3823fae4d6bf98f1871e8a5c2956a53300ff7
rh <ryanjharvey88@gmail.com>
2016-05-17 13:15:36 -0700

    Merge

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/geneology_test", "Ryan", "dirtyhands");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/geneology_test", null, null);
   }
 
   @Override
